a4169653064ca955705f90f6c224a40768069f1f
totaotata <totaotata@protonmail.com>
2019-01-10 14:45:09 +0100

    Ajout des commentaires

--- lib/01_pyramids.rb
+++ lib/01_pyramids.rb
@@ -46,23 +46,29 @@ end
     puts "Combien d'étages veux-tu ? (entre 1 et 25)"
     nb_etage = gets.chomp.to_i
 
+if (nb_etage%2!=0)
 
-  if (nb_etage > 0) && (nb_etage < 26)
+    if (nb_etage > 0) && (nb_etage < 26)
 
-      nb_etage = (nb_etage + 1)/2
-      nb_etage.times do |i|
-        i += 1
-      puts " " * (nb_etage-i) + "#" * (2*i-1) + " " * (nb_etage-i)
+        nb_etage = (nb_etage + 1)/2
+
+          nb_etage.times do |i|
+            i += 1          #incrémentation de i
+            puts " " * (nb_etage-i) + "#" * (2*i-1)  #  " " * (nb_etage-i)  == espacement || + "#" * (2*i-1) = dièse pour être en impair
+          end
+
+        nb_etage -= 1     # Décrémentation du i
+
+          nb_etage.times do |i|   #Incrémentation de i qui repart à zéro
+            puts " " * (1+i) + "#" * (2*nb_etage-2*i-1)   #puts " " * (1+i) == Espacement || "#" * (2*nb_etage-2*i-1) == nombre de dièse impair
+          end
+
+        else
+          puts "Le nombre d'étages doit être compris entre 1 et 25."
       end
 
-      nb_etage -= 1
-
-      nb_etage.times do |i|
-
-        puts " " * (1+i) + "#" * (2*nb_etage-2*i-1) + " " * (1+i)
-      end
-      else
-        puts "Le nombre d'étages doit être compris entre 1 et 25."
-      end
+  else
+    puts "Mauvais numéro demande à Toutânkhamon"
   end
+end
   wtf_pyramid
